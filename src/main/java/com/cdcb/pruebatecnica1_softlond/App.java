dc0f9eeebe623e500513f041aa9663b7d7338c3f
DavidCDCB <cristian-cb1@hotmail.com>
2023-03-31 06:51:48 -0500

    Inició con estructura de carpetas

---
+++ src/main/java/com/cdcb/pruebatecnica1_softlond/App.java
@@ -0,0 +1,11 @@
+package com.cdcb.pruebatecnica1_softlond;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+    }
+}
